86d9c8e9c8ca744526f1d8f4f227f853189dc580
RRajnishR <ravisai.rajnish@gmail.com>
2019-09-03 18:24:30 +0530

    Added 3-4 pages in Admin Dashboard

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,6 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 var router = express.Router();
 const checkAuthentication = require('../middlewares/admin.middleware');
+const examiner = require('../models/examiner.model');
+const userdb = require('../models/users.model');
+const readingSectionDB = require('../models/readingSection.model');
+const listeningSectionDB = require('../models/listeningSection.model');
+const writingSectionDB = require('../models/writingSection.model');
+const speakingSectionDB = require('../models/speakingSection.model');
+const examDB = require('../models/userResponse.model');
 
 const Language = require('../models/language.model');
 
@@ -51,23 +58,66 @@ router.post('/login', (req, res) => {
     This route will open up the dashboard. have to add a middleware to check if the session of admin 
     exists or not
 */
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', async(req, res) => {
+
+    const users = await userdb.find({});
+    const examiners = await examiner.find({});
+    const languages = await Language.find({});
+    const exams = await examDB.find({});
     res.render("adminviews/dashboard", {
         title: "Administrator's DashBoard",
         description : "This page will the admin to control the system",
-        keywords: "Control, online test portal, India Languages"
+        keywords: "Control, online test portal, India Languages",
+        successMessage : req.flash('successMessage'),
+        errorMessage : req.flash('errorMessage'),
+        users,
+        examiners,
+        languages,
+        exams
     });
 });
 /*
   This route will take you to the Examiners page, from here admin can see all the examiners and control them.  
 */
 router.get('/examiners', (req, res) => {
-    res.render("adminviews/examinerlist", {
-        title: "All Languages",
-        description : "This page will let the admin create different languages",
-        keywords: "Control, online test portal, India Languages"
+    examiner.find({}, (err, examiners) => {
+        res.render("adminviews/examinerlist", {
+            title: "All Examiners",
+            description : "This page will let the admin view all the examiners",
+            keywords: "Control, online test portal, India Languages",
+            examiners,
+            successMessage : req.flash('successMessage'),
+            errorMessage : req.flash('errorMessage'),
+        });
     });
 });
+
+router.get('/users', (req, res) => {
+    userdb.find({}, (err, users) => {
+        res.render("adminviews/userlist", {
+            title: "All Languages",
+            description : "This page will let the admin view all the users",
+            keywords: "Control, online test portal, India Languages",
+            users,
+            successMessage : req.flash('successMessage'),
+            errorMessage : req.flash('errorMessage'),
+        });
+    });
+});
+
+router.get('/exams', (req, res) => {
+    examDB.find({}, (err, exams) => {
+        res.render("adminviews/examlist", {
+            title: "All Exams",
+            description : "This page will let the admin view all the Exams",
+            keywords: "Control, online test portal, India Languages",
+            exams,
+            successMessage : req.flash('successMessage'),
+            errorMessage : req.flash('errorMessage'),
+        });
+    });
+});
+
 /*
   This route will take you to the Language page, from here admin can see all the languages and 
   CRUD Them.  
